2802cc8e71efe9e57e3266408bb9a2c06e8386dd
Benjamin S <cph-bs190@cphbusiness.dk>
2020-02-22 14:07:50 +0100

    Har tilføjet bedre loginsystem, unikke brugere kan kun logge ind en gang på tværs af browsere

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -7,7 +7,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 @WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
@@ -26,6 +28,11 @@ public class LoginServlet extends HttpServlet {
             servletContext.setAttribute("brugerMap", brugerMap);
         }
 
+        if(((Set<String>)servletContext.getAttribute("aktiveBrugere")) == null) {
+            Set<String> aktiveBrugere = new HashSet<>();
+            servletContext.setAttribute("aktiveBrugere", aktiveBrugere);
+        }
+
         if (!((Map<String, String>)servletContext.getAttribute("brugerMap")).containsKey(navn)) {
 
             request.setAttribute("besked",  "Opret ny bruger");
@@ -37,14 +44,17 @@ public class LoginServlet extends HttpServlet {
 
             if (navn.equals("admin")) {
             request.getRequestDispatcher("WEB-INF/Admin.jsp").forward(request, response);
-            } else {
+            }
+
+            if (!((Set<String>)servletContext.getAttribute("aktiveBrugere")).contains(navn)) {
+
+                ((Set<String>)servletContext.getAttribute("aktiveBrugere")).add(navn);
                 session.setAttribute("besked", "Du er logget på som " + navn);
                 request.getRequestDispatcher("WEB-INF/HuskeListe.jsp").forward(request, response);
             }
         }
 
-         // Gå til log-ind. dvs index siden.
-        request.setAttribute("besked",  "Koden var forkert. Prøv igen.");
+        request.setAttribute("besked",  "Noget gik galt. Prøv igen.");
         request.getRequestDispatcher("index.jsp").forward(request, response);
 
     }
